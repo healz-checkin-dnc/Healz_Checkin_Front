aecd5440695d895d8989160d27c9421d5dba9d94
Kevinn Vannucci <kevinnvannucci0@gmail.com>
2025-07-25 16:53:47 -0300

    atualizado pra deploy

--- src/services/handleForm.ts
+++ src/services/handleForm.ts
@@ -1,14 +1,11 @@
 import type { CheckinSchemaType } from '../schemas/checkinSchema';
 
 export default class HandleSubmit {
-  private static API_URL =
-    import.meta.env.MODE === 'development'
-      ? 'http://localhost:3001' //roda local
-      : '/.netlify/functions'; // roda na netlify
+  private static API_URL = import.meta.env.VITE_API_URL!;
 
   async execute(input: CheckinSchemaType): Promise<any> {
     try {
-      const res = await fetch(`${HandleSubmit.API_URL}/send-form`, {
+      const res = await fetch(`${HandleSubmit.API_URL}/api/send-form`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(input),
@@ -16,7 +13,9 @@ export default class HandleSubmit {
 
       if (!res.ok) {
         const errorBody = await res.text();
-        throw new Error(`Error submitting form: ${res.status} ${res.statusText} - ${errorBody}`);
+        throw new Error(
+          `Error submitting form: ${res.status} ${res.statusText} - ${errorBody}`
+        );
       }
 
       return await res.json();
