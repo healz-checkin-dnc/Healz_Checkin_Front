7b2ffa011b8fe62b2629a65405cb71587b3f81bf
Kevinn Vannucci <kevinnvannucci0@gmail.com>
2025-07-26 11:50:30 -0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/GlobalStyle';
-import CheckinPage from '../pages/CheckinPage';
+import CheckinPage from './pages/CheckinPage';
 import Preloader from './components/Preloader/Preloader';
 
 export function App() {
